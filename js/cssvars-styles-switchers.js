13c74d6de60eb3835cd586eea98ab3b3b66878e2
Majali <kim@majali.net>
2025-08-03 08:12:15 +0300

    Created cssvars-styles-switchers.js

---
+++ js/cssvars-styles-switchers.js
@@ -0,0 +1,18 @@
+const styles = ['none', 'brutal-style', 'brutal-lite-style', 'glass-style', 'retro-style', 'retro-light-style',
+                'minimalist-style', 'neon-style', 'cyberpunk-style', 'luxury-style', 'high-contrast-style',
+                'neumorphism-style', 'modern-style', 'playful-style', 'material-style', 'modular-style',
+                'flat-style', 'data-style', 'enterprise-style', 'system-ui-style', 'colorful-dashboard-style',
+]
+
+function changeStyle(){
+
+    let stylesElement = document.querySelector('#styles')
+    console.log(stylesElement)
+    console.log(stylesElement.value)
+    let bodyElement = document.querySelector('body')
+    
+    if (stylesElement.value != 'none'){
+        bodyElement.classList.value=''
+        bodyElement.classList.add(stylesElement.value)
+    }
+}
